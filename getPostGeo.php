3cb3168826f827620224214af7f89fb763aace70
Crane Johnson <crane@whitewinter.net>
2020-11-24 21:17:29 -0900

    updates

--- getPostGeo.php
+++ getPostGeo.php
@@ -1,8 +1,8 @@
 <?php
 /**
  * Description: This script gets messages from the specified mailbox and
- * drops the data into folder which then gets pushed over to AWIPS. 
- * Shef data will only be ingested 
+ * drops the data into folder which then gets pushed over to AWIPS.
+ * Shef data will only be ingested
  * into the local RFC shef encoder.
  *
  * This scripts requires php5 IMAP support. This was installed on redrock with
@@ -22,61 +22,15 @@ chdir(dirname(__FILE__));
 date_default_timezone_set('UTC');
 
 /* Include config file for paths etc..... */
-define("PROJECT_ROOT",dirname(__FILE__).'/../');
 #Private file
 define("CREDENTIALS_FILE","/usr/local/apps/scripts/bcj/hydroTools/login.php");
-define("LOG_TYPE","FILE");
-define("LOG_DIRECTORY",PROJECT_ROOT."log/");
-define("TEMP_DIRECTORY",PROJECT_ROOT."data/");
-define("TO_LDAD","/usr/local/apps/scripts/bcj/hydroTools/TO_LDAD/");
-define("SHEF_HEADER","SRAK58 PACR ".date('dHi')."\nACRRR3ACR \nWGET DATA REPORT \n\n");
-
-
-//Pear log package
-require_once ('../../lib/Pear/Log.php');
-//Pear cache_lite package
-require_once('../../lib/Pear/Cache/Lite.php');
 
 include_once(CREDENTIALS_FILE);
-
-/* Include config file for paths etc..... */
-$mysqli->select_db("aprfc");
-
-date_default_timezone_set('UTC');
-
-/**
- * Setup PEAR logging utility
- */
-
-//Console gets more information, file and sql only get info and above errors
-$consoleMask = Log::MAX(PEAR_LOG_DEBUG);
-$fileMask = Log::MAX(PEAR_LOG_INFO);
-$sqlMask = Log::MAX(PEAR_LOG_INFO);
-
-$sessionId = 'ID:'.time();
-
-if(LOG_TYPE == 'FILE'){
-    $script = basename(__FILE__, '.php');
-    $file = Log::factory('file',LOG_DIRECTORY.$script.'.log',$sessionId);
-    $file->setMask($fileMask);
-    $console = Log::factory('console','',$sessionId);
-    $console->setMask($consoleMask);
-    $logger = Log::singleton('composite');
-    $logger->addChild($console);
-    $logger->addChild($file);
-}
-if(LOG_TYPE == 'NULL'){
-    $logger = Log::singleton('null');
-}
-
 
 /**
  *
  *  MAIN PROGRAM LOGIC
  */
-
-$logger->log("END",PEAR_LOG_INFO);
-$sendshef = 0;
 
 
 #################Mailbox Configuration Settings########################
@@ -85,7 +39,7 @@ $password = GMAIL_PASSWORD;
 
 #//Which folders or label do you want to access? - Example: INBOX, All Mail, Trash, labelname
 #//Note: It is case sensitive
-$imapmainbox = "daveSnow";
+$imapmainbox = "geoPhoto";
 $messagestatus = "ALL";
 
 
@@ -97,76 +51,128 @@ $hostname = $imapaddress . $imapmainbox;
 
 $final_box = "trash";
 
-$verbose = false;
+/* try to connect */
+$inbox = imap_open($hostname,$username,$password) or die('Cannot connect to Gmail: ' . imap_last_error());
 
-$mbox = imap_open($hostname, $username,$password);
 
-if(!$mbox){
-    $logger->log("Could not open sheffile inbox ($imapmainbox in account $username) aborting....",PEAR_LOG_ERR);
-        exit();
+/* get all new emails. If set to 'ALL' instead
+ * of 'NEW' retrieves all the emails, but can be
+ * resource intensive, so the following variable,
+ * $max_emails, puts the limit on the number of emails downloaded.
+ *
+ */
+$emails = imap_search($inbox,'ALL');
+
+/* useful only if the above search is set to 'ALL' */
+$max_emails = 16;
+
+
+/* if any emails found, iterate through each email */
+if($emails) {
+
+    $count = 1;
+
+    /* put the newest emails on top */
+    rsort($emails);
+
+    /* for every email... */
+    foreach($emails as $email_number)
+    {
+
+        /* get information specific to this email */
+        $overview = imap_fetch_overview($inbox,$email_number,0);
+
+        /* get mail message */
+        $message = imap_fetchbody($inbox,$email_number,2);
+
+        /* get mail structure */
+        $structure = imap_fetchstructure($inbox, $email_number);
+
+        $attachments = array();
+
+        /* if any attachments found... */
+        if(isset($structure->parts) && count($structure->parts))
+        {
+            for($i = 0; $i < count($structure->parts); $i++)
+            {
+                $attachments[$i] = array(
+                    'is_attachment' => false,
+                    'filename' => '',
+                    'name' => '',
+                    'attachment' => ''
+                );
+
+                if($structure->parts[$i]->ifdparameters)
+                {
+                    foreach($structure->parts[$i]->dparameters as $object)
+                    {
+                        if(strtolower($object->attribute) == 'filename')
+                        {
+                            $attachments[$i]['is_attachment'] = true;
+                            $attachments[$i]['filename'] = $object->value;
+                        }
+                    }
+                }
+
+                if($structure->parts[$i]->ifparameters)
+                {
+                    foreach($structure->parts[$i]->parameters as $object)
+                    {
+                        if(strtolower($object->attribute) == 'name')
+                        {
+                            $attachments[$i]['is_attachment'] = true;
+                            $attachments[$i]['name'] = $object->value;
+                        }
+                    }
+                }
+
+                if($attachments[$i]['is_attachment'])
+                {
+                    $attachments[$i]['attachment'] = imap_fetchbody($inbox, $email_number, $i+1);
+
+                    /* 4 = QUOTED-PRINTABLE encoding */
+                    if($structure->parts[$i]->encoding == 3)
+                    {
+                        $attachments[$i]['attachment'] = base64_decode($attachments[$i]['attachment']);
+                    }
+                    /* 3 = BASE64 encoding */
+                    elseif($structure->parts[$i]->encoding == 4)
+                    {
+                        $attachments[$i]['attachment'] = quoted_printable_decode($attachments[$i]['attachment']);
+                    }
+                }
+            }
+        }
+
+        /* iterate through each attachment and save it */
+        foreach($attachments as $attachment)
+        {
+            if($attachment['is_attachment'] == 1)
+            {
+                $filename = $attachment['name'];
+                if(empty($filename)) $filename = $attachment['filename'];
+
+                if(empty($filename)) $filename = time() . ".dat";
+
+                /* prefix the email number to the filename in case two emails
+                 * have the attachment with the same file name.
+                 */
+                $fp = fopen($email_number . "-" . $filename, "w+");
+                fwrite($fp, $attachment['attachment']);
+                fclose($fp);
+            }
+
+        }
+
+        if($count++ >= $max_emails) break;
+    }
+
 }
 
-#####Spit out the Total Number of Messages from Iridium
+/* close the connection */
+imap_close($inbox);
 
-$check = imap_check($mbox);
-
-$sbdmes = $check->Nmsgs;
-
-$logger->log("$sbdmes total messages in sheffile inbox",PEAR_LOG_INFO);
-
-$numnew =  imap_num_recent($mbox);
-
-
-$data = SHEF_HEADER;
-$numData = 0;
-
-######Process each message
-$emails = imap_search($mbox,'ALL');
-if($emails){
-    arsort($emails); //JUST DO ARSORT
-    foreach($emails as $email_number) {
-        $sitedata = array();
-        $msgno = $email_number;
-        $text = "";
-        ######Get the message header information
-        $header = imap_header($mbox,$msgno);
-        $date = strtotime($header->date);
-        ######Get the file name and parse out the datestamp
-        $string = imap_body($mbox,$msgno);
-        $lines = preg_split('/$\R?^/m', $string);
-        $snowDepth = 0;
-        $id = '';
-        foreach($lines as $line){ 
-            if(strpos($line,':') == false ) continue;
-            $parts = explode(':',$line);
-            if( strpos($parts[0],'NWSLI') !== false ) $id = trim($parts[1]);
-            if( strpos($parts[0],'SD') !== false ) {
-                $numData = $numData + 1;
-                $snowDepth = trim($parts[1]);
-                # Example shef: AR SIXA2 180306 Z DH0515/DC1803072333/HGIRZ -9999
-                $dc = date('\D\CymdHi');
-                $shefData = ".AR ".$id." ".date('ymd \Z \D\HHi',$date)."/$dc/SDIRZ $snowDepth\n";
-                $shefData = trim($shefData);
-                $logger->log("shef data:$shefData",PEAR_LOG_INFO);
-                if(substr($shefData, 0, 3 ) === ".AR")  $data .= $shefData."\n";
-            }   
-        }
-        imap_delete($mbox, $msgno);
-        imap_expunge($mbox);
-    }
-}  #Outer if loop
-
-
-if($numData> 0){
-    $filename = 'sheffile.'.date('ymdHi');
-    file_put_contents(TEMP_DIRECTORY.$filename, $data);
-    if(file_put_contents(TO_LDAD.$filename, $data)){
-        $logger->log("Moved shef data to LDAD",PEAR_LOG_INFO);
-    }    
-}  #If data loop
-
-$logger->log("END",PEAR_LOG_INFO);
-
+echo "Done";
 
 ?>
 
