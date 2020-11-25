db807c75c6bc205eee0e5e5d35d3646450914d35
Crane Johnson <crane@whitewinter.net>
2020-11-24 18:12:07 -0900

    minor update

--- geoTag_Photos.py
+++ geoTag_Photos.py
@@ -582,7 +582,7 @@ def main():
     #for each line in the input file
     for line in fin:
       #read replace the string and write to output file
-      fout.write(line.replace('<GEOJSON_PHOTOS>', json.dumps(geoJsonCollection)))
+      fout.write(line.replace('"<GEOJSON_PHOTOS>"', json.dumps(geoJsonCollection)))
     #close input and output files
     fin.close()
     fout.close()
